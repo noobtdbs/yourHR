ce50d274a60f2aca67071067cc8f31862d1ce1e3
Tridibesh Misra <tridibeshmisra2002@gmail.com>
2024-08-27 23:53:10 +0530

    adding comments in config folder

--- backend/config/CloudinaryConfig.js
+++ backend/config/CloudinaryConfig.js
@@ -1,11 +1,11 @@
 // server/config/cloudinaryConfig.js
 const cloudinary = require('cloudinary').v2;
 
-console.log('Cloudinary Config:', {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// console.log('Cloudinary Config:', {
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
